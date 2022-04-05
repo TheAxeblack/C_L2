e455a682de449a0000a9309f6a07b3e32cb4b319
Mathis Lecuyer <mathis.lecuyer.pro@gmail.com>
2022-04-05 13:21:12 +0200

    Auto stash before rebase of "origin/main"

--- Partie2/TP/tp3/main.c
+++ Partie2/TP/tp3/main.c
@@ -4,10 +4,10 @@ int main(int argc, char **argv) {
     liste liste1 = liste_vide();
     pile pile1 = pile_vide();
     file file1 = file_vide();
+
     test_module_liste(liste1);
     test_module_pile(pile1);
     test_module_file(file1);
-
 
     return (0);
 }
--- Partie2/TP/tp3/tp3.h
+++ Partie2/TP/tp3/tp3.h
@@ -19,12 +19,5 @@ pile test_module_pile(pile p);
 
 file test_module_file(file f);
 
-/* Fonctions de l'exercice 2 */
-
-liste ajout_message(file f, element msg);
-
-file supprime_message(file f);
-
-void affiche_message(file f);
 
 #endif
