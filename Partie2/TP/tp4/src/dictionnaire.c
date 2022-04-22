626a2168b2476c21214bf50f832bc2fbb5b9a370
TheAxeblack <mathis.lecuyer.pro@gmail.com>
2022-04-22 14:32:01 +0200

    maj existe

--- Partie2/TP/tp4/src/dictionnaire.c
+++ Partie2/TP/tp4/src/dictionnaire.c
@@ -1,10 +1,10 @@
 #include "../inc/dictionnaire.h"
 
-int existe_aux(dictionnaire d, char *mot, int *position) {
+int existe_aux(dictionnaire d, char *mot, int position) {
     if (!est_vide(d)) {
-        if (d->noeud != 0 && mot[*position] != '\0') {
-            if (mot[*position] == d->noeud) {
-                (*position)++;
+        if (d->noeud != 0 && mot[position] != '\0') {
+            if (mot[position] == d->noeud) {
+                position++;
                 existe_aux(d->gauche, mot, position);
             } else
                 existe_aux(d->droit, mot, position);
@@ -16,7 +16,6 @@ int existe_aux(dictionnaire d, char *mot, int *position) {
 
 int existe(dictionnaire d, char *mot) {
     int acc = 0;
-    int *ptr = &acc;
-    return existe_aux(d, mot, ptr);
+    return existe_aux(d, mot, acc);
 }
 
