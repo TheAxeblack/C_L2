939362d3b0bb7a847b33623368b5ba639689f4a7
TheAxeblack <mathis.lecuyer.pro@gmail.com>
2022-12-07 15:17:10 +0100

    reindent + delete useless files

--- Partie1/TP/tp3/nb_mystere_mguess.c
+++ Partie1/TP/tp3/nb_mystere_mguess.c
@@ -4,72 +4,69 @@
 
 void ft_usage(char *nom)
 {
-  printf("Usage %s : <entier> qui doit etre entre 0 et 100.\n", nom);
+	printf("Usage %s : <entier> qui doit etre entre 0 et 100.\n", nom);
 }
 
-void ft_kbuffer(void) {
-  int x;
+void ft_kbuffer(void)
+{
+	int x;
 
-  x = 0;
-  while (x != '\n' && x != EOF)
-    x = getchar();
+	x = 0;
+	while (x != '\n' && x != EOF)
+		x = getchar();
 }
-
-
 
 int main(int argc, char **argv)
 {
-  int n;
-  int x;
-  int i;
-  (void)argc;
+	int n;
+	int x;
+	int i;
+	(void)argc;
 
-  i = 1;
-  srand(time(NULL));
+	i = 1;
+	srand(time(NULL));
 
-  do
-    {
-      printf("Entrez un entier que la machine devra trouver\n");
-      if ((scanf("%d", &n) != 1) || n < 0 || n > 100)
+	do
 	{
-	  ft_usage(argv[0]);
-	  ft_kbuffer();
-	  exit(EXIT_FAILURE);
-	}
-      else
-	{
-	  printf("L'utilisateur a choisi un nombre mystere entre 0 et 100, a la machine de le trouver\n");
-	  x = 0;
-	  while (x != n)
-	    {
-	      
-	      printf("Proposition de la machine ? ");
-	      x = rand() % 100;
-	      printf("%d\n", x);
-	      if (x < n)
+		printf("Entrez un entier que la machine devra trouver\n");
+		if ((scanf("%d", &n) != 1) || n < 0 || n > 100)
 		{
-		  printf("C'est faux, le nombre mystere est superieur a %d\n", x);
-		  i++;
+			ft_usage(argv[0]);
+			ft_kbuffer();
+			exit(EXIT_FAILURE);
 		}
-	      else if (x > n)
+		else
 		{
-		  printf("C'est faux, le nombre mystere est inferieur a %d\n", x);
-		  i++;
+			printf("L'utilisateur a choisi un nombre mystere entre 0 et 100, a la machine de le trouver\n");
+			x = 0;
+			while (x != n)
+			{
+
+				printf("Proposition de la machine ? ");
+				x = rand() % 100;
+				printf("%d\n", x);
+				if (x < n)
+				{
+					printf("C'est faux, le nombre mystere est superieur a %d\n", x);
+					i++;
+				}
+				else if (x > n)
+				{
+					printf("C'est faux, le nombre mystere est inferieur a %d\n", x);
+					i++;
+				}
+				else
+				{
+					printf("C'est juste, la machine a trouver le nombre mystere (%d) en %d etape", n, i);
+					if (i == 1)
+						printf("\n");
+					else
+						printf("s\n");
+				}
+			}
 		}
-	      else
-		{
-		  printf("C'est juste, la machine a trouver le nombre mystere (%d) en %d etape", n, i);
-		  if (i == 1)
-		    printf("\n");
-		  else
-		    printf("s\n");
-		}
-	    }
-	}
-      printf("Voulez-vous recommencer ? (oui:1 / non:0)\n");
-      ft_kbuffer();
-    }
-  while ((n = getchar()) != '0');
-  exit(EXIT_SUCCESS);
+		printf("Voulez-vous recommencer ? (oui:1 / non:0)\n");
+		ft_kbuffer();
+	} while ((n = getchar()) != '0');
+	exit(EXIT_SUCCESS);
 }
-	
