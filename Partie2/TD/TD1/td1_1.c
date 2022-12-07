6c5c1700f5b6d36824d5a9ec1a3853bc0acbbe9e
TheAxeblack <mathis.lecuyer.pro@gmail.com>
2022-12-07 15:39:10 +0100

    reindent

--- Partie2/TD/TD1/td1_1.c
+++ Partie2/TD/TD1/td1_1.c
@@ -1,22 +1,22 @@
 /**
- * Programme calculant la différence entre la moyenne arithmétique et 
- * géométrique de 2 nombres
+ * Programme calculant la diffï¿½rence entre la moyenne arithmï¿½tique et
+ * gï¿½omï¿½trique de 2 nombres
  *
  * Version utilisant une fonction pour le calcul de la difference
  *
  * Cette version prend les valeurs sur la ligne de commande
- * les versions avec valeurs fixées ou lisant les valeurs au clavier
- * sont indiquées en commentaires en bas du fichier
+ * les versions avec valeurs fixï¿½es ou lisant les valeurs au clavier
+ * sont indiquï¿½es en commentaires en bas du fichier
  *
- * historique: 
+ * historique:
  *  creation le 23/01/2014 - A. Habrard
  *  MAJ      le 19/01/2018 - A. Habrard - modif usage
- *           le 24/01/2022 - A. Habrard - modif verification positivité nombres donnés en ligne de commande + usage
-                                        - modif verification scanf 
+ *           le 24/01/2022 - A. Habrard - modif verification positivitï¿½ nombres donnï¿½s en ligne de commande + usage
+                                        - modif verification scanf
  *
  * Note:
- *il n'est pas utile de prendre la valeur absolue car 
- *(sqrt(a)-sqrt(b))^2=a+b-2sqrt(a*b)>=0 et donc (a+b)/2>=sqrt(a*b) 
+ *il n'est pas utile de prendre la valeur absolue car
+ *(sqrt(a)-sqrt(b))^2=a+b-2sqrt(a*b)>=0 et donc (a+b)/2>=sqrt(a*b)
  */
 
 #include <stdio.h>
@@ -25,55 +25,54 @@
 
 /*Declaration de constantes*/
 
-
 /*declaration fonction */
 void calcule_difference(double a, double b);
 
 void usage(char *s);
 
-
 /*definition fonction usage */
-void usage(char *s) {
-    fprintf(stderr,
-            "Erreur usage: %s a b\n\ta et b 2 reels positifs, calcule la difference entre la moyenne arithmetique et geometrique de a et b\n",
-            s);
+void usage(char *s)
+{
+  fprintf(stderr, "Erreur usage: %s a b\n\ta et b 2 reels positifs, calcule la difference entre la moyenne arithmetique et geometrique de a et b\n", s);
 }
 
 /*definition fonction calcule_difference*/
-void calcule_difference(double a, double b) {
-    double moyenne_a = (a + b) / 2.0;
-    double moyenne_g = sqrt(a * b);
+void calcule_difference(double a, double b)
+{
+  double moyenne_a = (a + b) / 2.0;
+  double moyenne_g = sqrt(a * b);
 
-    double diff = moyenne_a - moyenne_g;
+  double diff = moyenne_a - moyenne_g;
 
-    printf("Moyenne arithmetique: %.4f, moyenne geometrique: %lf,\nla difference entre les 2 moyennes est %lf\n",
-           moyenne_a, moyenne_g, diff);
+  printf("Moyenne arithmetique: %.4f, moyenne geometrique: %lf,\nla difference entre les 2 moyennes est %lf\n",
+         moyenne_a, moyenne_g, diff);
 }
-
 
 /* Fonction main */
-int main(int argc, char *argv[]) {
-    double a, b;
+int main(int argc, char *argv[])
+{
+  double a, b;
 
-    if (argc != 3) {
-        usage(argv[0]);
-        return EXIT_FAILURE;
-    }
+  if (argc != 3)
+  {
+    usage(argv[0]);
+    return EXIT_FAILURE;
+  }
 
-    a = atof(argv[1]);
-    b = atof(argv[2]);
+  a = atof(argv[1]);
+  b = atof(argv[2]);
 
-    if (a >= 0 && b >= 0)
-        calcule_difference(a, b);
-    else {
-        usage(argv[0]);
-        printf("L'un des nombres au moins est negatif, a:%f, b:%f\n", a, b);
-        exit(EXIT_FAILURE);
-    }
+  if (a >= 0 && b >= 0)
+    calcule_difference(a, b);
+  else
+  {
+    usage(argv[0]);
+    printf("L'un des nombres au moins est negatif, a:%f, b:%f\n", a, b);
+    exit(EXIT_FAILURE);
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
-
 
 /* ---- */
 /* Fonction main, avec lecture clavier */
@@ -81,7 +80,7 @@ int main(int argc, char *argv[]) {
 int main(int argc,char * argv[])
 {
   double a,b;
-  
+
   printf("Entrez le nombre reel a :\n");
   if(scanf("%lf",&a)!=1){
      fprintf(stderr,"Erreur saisie nombre a\n");
@@ -105,12 +104,10 @@ int main(int argc,char * argv[])
 }
 */
 
-
 /* ----- */
-/* avec nombres fixes : on les met en constantes au début*/
+/* avec nombres fixes : on les met en constantes au dï¿½but*/
 /*#define VALEUR_A 5 */
 /*#define VALEUR_B 6  */
-
 
 /* Fonction main nombres fixes */
 /*
@@ -127,7 +124,7 @@ int main(int argc,char * argv[])
 /*
 int main(int argc,char * argv[])
 {
-  const int val_a=5; 
+  const int val_a=5;
   const int val_b=6;
   calcule_difference(val_a,val_b);
 
@@ -144,7 +141,7 @@ int main(int argc,char * argv[])
   {
     usage(argv[0]);
     return EXIT_FAILURE;
-  }else{  
+  }else{
 
     const double a=atof(argv[1]);
     const double b=atof(argv[2]);
@@ -161,4 +158,3 @@ int main(int argc,char * argv[])
   return EXIT_SUCCESS;
 }
 */
-
